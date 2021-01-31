5f53173f6d4d4ebbb60a8b89c40bf46ab528a1bb
Nico <nico.schikofsky@t-online.de>
2021-01-31 13:12:36 +0100

    open + save work now

--- gui.py
+++ gui.py
@@ -1,57 +1,66 @@
-import tkinter as tk
-from tkinter.filedialog import askopenfile, asksaveasfile
+from tkinter import *
 from PIL import Image, ImageTk
+from tkinter.filedialog import askopenfile, asksaveasfile
+from tkinter import messagebox
 
-root = tk.Tk()
+class Window(Frame):
+    def __init__(self, master=None):
+        Frame.__init__(self, master)
+        self.master = master
+        self.pack(fill=BOTH, expand=1)
+        self.image = None
+        
+        #Logo
+        load = Image.open("logo.png")
+        render = ImageTk.PhotoImage(load)
+        img = Label(self, image=render)
+        img.image = render
+        img.place(x=100, y=10)
 
-canvas = tk.Canvas(root, width=400, height=600)
-canvas.grid(columnspan=5, rowspan=5)
+        #Instructions
+        instructions = Label(master, text="Select a Image on your computer to resize it", font="Raleway")
+        instructions.place(x=55, y=80)
 
-#logo
-logo = Image.open('logo.png')
-logo = ImageTk.PhotoImage(logo)
-logo_label = tk.Label(image=logo)
-logo_label.image = logo
-logo_label.grid(column=1, row=0)
+        #browse button
+        browse_text = StringVar()
+        browse_btn = Button(master, textvariable=browse_text, command=lambda:open_file(self), font="Raleway", bg="#e20074", fg="white", height=2, width=15)
+        browse_text.set("Browse")
+        browse_btn.place(x=59, y=120)
 
-#instructions
-instructions = tk.Label(root, text="Select a Imgae on your computer to resize it", font="Raleway")
-instructions.grid(columnspan=5, column=0, row=1)
+        #save button
+        save_text = StringVar()
+        save_btn = Button(master, textvariable=save_text, command=lambda:save_file(self), font="Raleway", bg="#e20074", fg="white", height=2, width=15)
+        save_text.set("Save")
+        save_btn.place(x=214, y=120)
 
+        def open_file(self):
+            browse_text.set("loading...")
+            file = askopenfile(parent=master, mode='rb', title="Choose a image file", filetype=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif','*.*'])])
+            if file:
+                image = Image.open(file)
+                newsize = (300,300)
+                self.image = image.resize(newsize)
+                browse_text.set("Browse")
+                
+                #Display Image on screen
+                render = ImageTk.PhotoImage(self.image)
+                img = Label(image=render)
+                img.image = render
+                img.place(x=55, y=200)
 
-def open_file():
-        browse_text.set("loading...")
-        file = askopenfile(parent=root, mode='rb', title="Choose a image file", filetype=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif','*'])])
-        if file:
-            read_image = Image.open(file)
-            browse_text.set("Browse")
-
-            #Resize image to 300x300
-            newsize = (300, 300)
-            resize_image = read_image.resize(newsize)
-            
-            #Display Image on screen
-            rs_image = ImageTk.PhotoImage(resize_image)
-            rs_label = tk.Label(image=rs_image)
-            rs_label.image = rs_image
-            rs_label.grid(column=2, row=3)
-                      
-
-
-#browse button
-browse_text = tk.StringVar()
-browse_btn = tk.Button(root, textvariable=browse_text, command=lambda:open_file(), font="Raleway", bg="#e20074", fg="white", height=2, width=15)
-browse_text.set("Browse")
-browse_btn.grid(column=1, row=2)
-
-#save button
-save_text = tk.StringVar()
-save_btn = tk.Button(root, textvariable=save_text, command=lambda:save_file(), font="Raleway", bg="#e20074", fg="white", height=2, width=15)
-save_text.set("Save")
-save_btn.grid(column=2, row=2)
-
-canvas = tk.Canvas(root, width=600, height=250)
-canvas.grid(columnspan=3)
-
-
+        def save_file(self):
+            if self.image:
+                save_text.set("loading...")
+                files = [('GIF', '*.gif'), ('PNG', '*.png'), ('JPG', '*.jpg'),('All Files', '*.*')] 
+                file = asksaveasfile( initialfile=self.image_name, title="Save Resized Image", filetypes = files, defaultextension = files) 
+                
+                print("file.name: ", file.name)
+                if file:
+                    #self.image.save("test.gif", format="gif")
+                    self.image.save(file.name)
+                    save_text.set("Image saved")
+root = Tk()
+app = Window(root)
+root.wm_title("Resize Image")
+root.geometry("400x530")
 root.mainloop()
