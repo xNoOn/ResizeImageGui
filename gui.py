fba462d298185fdfbc310705fc58edd8d6ec7079
Nico <nico.schikofsky@t-online.de>
2021-02-01 19:42:45 +0100

    2 new labels: length, witdh

--- gui.py
+++ gui.py
@@ -1,6 +1,7 @@
 import tkinter as tk
-from tkinter import Label, Button, StringVar, BOTH, Frame, messagebox
+from tkinter import Label, Button, StringVar, BOTH, Frame, messagebox, Entry
 from tkinter.filedialog import askopenfile, asksaveasfile
+import tkinter.font as tkFont
 from PIL import Image, ImageTk
 
 
@@ -10,6 +11,9 @@ class Window(Frame):
         self.master = master
         self.pack(fill=BOTH, expand=1)
         self.image = None
+        self.imageLength = None
+        self.imageWidth = None
+        self.imageSize = None
         
         #Logo
         load = Image.open("C:/Users/nicos/OneDrive/Projekte/Dev/ImageResize/logo.png")
@@ -34,21 +38,37 @@ class Window(Frame):
         save_text.set("Save")
         save_btn.place(x=214, y=120)
 
+        #Input Label -> Length / Width
+        #Label(master, text="Length").place(x=59, y=185)
+        fontStyleLabel = tkFont.Font(family="Courier New", size=15)
+        fontStyleEntry = tkFont.Font(family="Courier New", size=12)
+
+        length = StringVar(master, value="300")
+        input_length = Entry(master, width=23, textvariable=length, font=fontStyleEntry)
+        input_length.place(x=59, y=185)
+        Label(master, text="X", font=fontStyleLabel).place(x=198, y=181)
+        width = StringVar(master, value="300")
+        input_width = Entry(master, width=14, textvariable=width, font=fontStyleEntry)
+        input_width.place(x=214, y=185)
+
+
         def open_file(self):
             try:
                 browse_text.set("loading...")
                 file = askopenfile(parent=master, mode='rb', title="Choose a image file", filetype=[('Image Files', ['.jpeg', '.jpg', '.png', '.gif','*.*'])])
                 if file:
                     image = Image.open(file)
-                    newsize = (300,300)
-                    self.image = image.resize(newsize)
+                    #Get input length + input width
+                    getImageSize(self)
+                    #Resize Image
+                    self.image = image.resize(self.imageSize)
                     browse_text.set("Browse")
-                    
+
                     #Display Image on screen
                     render = ImageTk.PhotoImage(self.image)
                     img = Label(image=render)
                     img.image = render
-                    img.place(x=55, y=200)
+                    img.place(x=57, y=220)
             except Exception as ex:
                 messagebox.showerror("Error","Something went wrong, try again please!")
                 browse_text.set("Browse")
@@ -79,14 +99,24 @@ class Window(Frame):
                 for widget in self.master.winfo_children():
                     if widget.widgetName == "label":  
                         labelcount += 1
-                        if labelcount == 2:   
+                        if labelcount == 3:   
                             widget.destroy()
             except Exception as ex:
+                print(ex)
+
+        def getImageSize(self):
+            try:
+                self.imageLength = int(length.get())
+                self.imageWidth = int(width.get())
+                self.imageSize = (self.imageLength, self.imageWidth)
+            except Exception as ex:
+                browse_text.set("Browse")
+                messagebox.showerror("Error","Please enter a numeric number!")
                 print(ex)
 
 root = tk.Tk()
 app = Window(root)
 root.wm_title("Resize Image")
 root.iconbitmap("C:/Users/nicos/OneDrive/Projekte/Dev/ImageResize/favicon.ico")
-root.geometry("400x530")
+root.geometry("400x540")
 root.mainloop()
